b9ef6fcd1ac07567bcfc5bdba1059cec84c50435
Artem Feoktistov <afeoktistov@tango.me>
2017-09-14 16:49:12 +0300

    Added HelloWorld example

---
+++ src/unity3d/Grpc.Unity/Assets/Testing/Launch.cs
@@ -0,0 +1,25 @@
+﻿//
+// Launch.cs
+//
+// Created by Artem Feoktistov <afeoktistov@tango.me>
+//
+// Copyright (c) 2017 TangoMe, Inc.
+// All Rights Reserved.
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+  void Start ()
+  {
+    GreeterServer.Start ();
+  }
+
+
+  public void OnStartClient()
+  {
+    GreeterClient.Start ();
+  }
+}
